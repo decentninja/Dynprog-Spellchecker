363bfd3e16862ea3a6668c018ae8117df00d8b10
sootn <encrypt.wave@gmail.com>
2014-09-24 14:06:55 +0200

    Distance matrix generator

---
+++ matrix.java
@@ -0,0 +1,37 @@
+/* Labb 2 i DD1352 Algoritmer, datastrukturer och komplexitet    */
+/* Se labbanvisning under kurswebben https://www.kth.se/social/course/DD1352 */
+/* Ursprunglig författare: Viggo Kann KTH viggo@nada.kth.se      */
+import java.util.LinkedList;
+import java.util.List;
+
+class ClosestWords {
+
+  static int partDist(String w1, String w2, int w1len, int w2len) {
+    if (w1len == 0)
+      return w2len;
+    if (w2len == 0)
+      return w1len;
+    int res = partDist(w1, w2, w1len - 1, w2len - 1) + 
+	(w1.charAt(w1len - 1) == w2.charAt(w2len - 1) ? 0 : 1);
+    int addLetter = partDist(w1, w2, w1len - 1, w2len) + 1;
+    if (addLetter < res)
+      res = addLetter;
+    int deleteLetter = partDist(w1, w2, w1len, w2len - 1) + 1;
+    if (deleteLetter < res)
+      res = deleteLetter;
+    return res;
+  }
+
+  public static void main(String [] args) {
+    String a = "labi";
+    String b = "blad";
+    System.out.println(a + " " + b);
+    for(int i = 0; i < 5; i++) {
+      for(int j = 0; j < 5; j++) {
+        System.out.print(partDist(a, b, j, i) + " ");
+      }
+      System.out.println("");
+    }
+  }
+
+}
